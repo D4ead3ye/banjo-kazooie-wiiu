2622067e2ff6dbdd7c5a43f0d631bc2c1fa8b0a5
Malkierian <malkierian@live.com>
2025-10-23 17:29:03 -0700

    Update to Torch working on Windows. Change port archive name.

---
+++ include/bk_string.h
@@ -0,0 +1,21 @@
+#ifndef BK_STRING_H
+#define BK_STRING_H
+
+#if 0
+#include <ultra64.h>
+#else
+#include <libultraship/libultra/types.h>
+#endif
+
+/* Custom string functions for Banjo-Kazooie */
+void strcatc(char *dst, char src);
+void strFToA(char *dst, f32 val);
+void _strFToA(char *dst, f32 val, s32 decPlaces);
+void strIToA(char *str, s32 num);
+void _strIToA(char *str, s32 num, char prefix);
+s32 strcmpToTok(char *str1, char* str2, char* str3);
+char *strtok(char *str, const char *delim);
+void strcpyToTok(char *arg0, char *arg1, char *arg2);
+void strToUpper(char *str);
+
+#endif /* BK_STRING_H */
