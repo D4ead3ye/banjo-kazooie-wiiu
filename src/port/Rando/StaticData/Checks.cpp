92c056db508652fb117973af5f69ea78c43f0629
Caladius <clatini88@gmail.com>
2026-04-14 11:26:51 -0400

    Remove Checks comments

--- src/port/Rando/StaticData/Checks.cpp
+++ src/port/Rando/StaticData/Checks.cpp
@@ -23,11 +23,11 @@ std::map<RandoCheckId, RandoStaticCheck> Checks = {
     RC(RC_MM_JIGGY_CHIMPY,                              RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_9_MM_CHIMPY,                              -5599, -20, 5599),
     RC(RC_MM_JIGGY_CONGA,                               RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_A_MM_CONGA,                               -3758, -172, 5028),
     RC(RC_MM_JIGGY_HILL,                                RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_07_MM_HILL,                               4335, 1485, 794),
-    RC(RC_MM_JIGGY_HUTS,                                RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_5_MM_HUTS,                                3319, 2165, -1410), //This one changes
-    RC(RC_MM_JIGGY_JINJO,                               RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_01_MM_JINJO,                              1702, -41, 2941), //This one changes
+    RC(RC_MM_JIGGY_HUTS,                                RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_5_MM_HUTS,                                3319, 2165, -1410),
+    RC(RC_MM_JIGGY_JINJO,                               RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_01_MM_JINJO,                              1702, -41, 2941),
     RC(RC_MM_JIGGY_JUJU,                                RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_4_MM_JUJU,                                4309, 2374, -1487),
     RC(RC_MM_JIGGY_MUMBOS_SKULL,                        RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_03_MM_MUMBOS_SKULL,                       5580, 2756, -2708),
-    RC(RC_MM_JIGGY_ORANGE_PADS,                         RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_8_MM_ORANGE_PADS,                         -3804, -198, 5036), //This one changes
+    RC(RC_MM_JIGGY_ORANGE_PADS,                         RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_8_MM_ORANGE_PADS,                         -3804, -198, 5036),
     RC(RC_MM_JIGGY_RUINS,                               RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_06_MM_RUINS,                              -3699, 2438, -2113),
     RC(RC_MM_JIGGY_TICKERS_TOWER,                       RCTYPE_JIGGY,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_46_JIGGY,             JIGGY_02_MM_TICKERS_TOWER,                      373, 4221, -906),
     RC(RC_MM_JINJO_BLUE,                                RCTYPE_JINJO,           LEVEL_1_MUMBOS_MOUNTAIN,    ACTOR_60_JINJO_BLUE,        NULL,                                           1702, -125, 2941),
