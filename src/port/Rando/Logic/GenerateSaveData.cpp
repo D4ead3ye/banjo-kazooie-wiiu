001885cc2a5ec1a8b0c3e3991283b35151ea550b
Caladius <clatini88@gmail.com>
2026-05-12 15:01:12 -0400

    Add Starting Loadout Options

--- src/port/Rando/Logic/GenerateSaveData.cpp
+++ src/port/Rando/Logic/GenerateSaveData.cpp
@@ -1,9 +1,47 @@
 #include "Logic.h"
 #include "port/ui/Notification.h"
 #include <libultraship/bridge/consolevariablebridge.h>
+#include "port/ui/cvar_prefixes.h"
 #include "port/enhancements/events/hooks/Events.h"
 
-#include "spdlog/spdlog.h"
+#include "port/save/Types.h"
+
+extern "C" {
+void ability_setLearned(s32 move, s32 val);
+void ability_setHasUsed(enum ability_e move);
+
+void item_setMaxCount(s32 item);
+}
+
+// clang-format off
+std::map<ability_e, std::pair<const char*, const char*>> abilityLoadoutMap = {
+    { ABILITY_0_BARGE,              { "Beak Barge",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBarge") } },
+    { ABILITY_1_BEAK_BOMB,          { "Beak Bomb",      CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBomb") } },
+    { ABILITY_2_BEAK_BUSTER,        { "Beak Buster",    CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBuster") } },
+    { ABILITY_3_CAMERA_CONTROL,     { "Camera Control", CVAR_RANDOMIZER_SETTING("Loadout.Abilities.CameraControl") } },
+    { ABILITY_4_CLAW_SWIPE,         { "Claw Swipe",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.ClawSwipe") } },
+    { ABILITY_5_CLIMB,              { "Climb",          CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Climb") } },
+    { ABILITY_6_EGGS,               { "Eggs",           CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Eggs") } },
+    { ABILITY_7_FEATHERY_FLAP,      { "Feathery Flap",  CVAR_RANDOMIZER_SETTING("Loadout.Abilities.FeatheryFlap") } },
+    { ABILITY_8_FLAP_FLIP,          { "Flap Flip",      CVAR_RANDOMIZER_SETTING("Loadout.Abilities.FlapFlip") } },
+    { ABILITY_9_FLIGHT,             { "Flight",         CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Flight") } },
+    { ABILITY_A_HOLD_A_JUMP_HIGHER, { "Jump Higher",    CVAR_RANDOMIZER_SETTING("Loadout.Abilities.JumpHigher") } },
+    { ABILITY_B_RATATAT_RAP,        { "Rat-a-tat Rap",  CVAR_RANDOMIZER_SETTING("Loadout.Abilities.RatatatRap") } },
+    { ABILITY_C_ROLL,               { "Roll",           CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Roll") } },
+    { ABILITY_D_SHOCK_JUMP,         { "Shock Jump",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.ShockJump") } },
+    { ABILITY_E_WADING_BOOTS,       { "Wading Boots",   CVAR_RANDOMIZER_SETTING("Loadout.Abilities.WadingBoots") } },
+    { ABILITY_F_DIVE,               { "Dive",           CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Dive") } },
+    { ABILITY_10_TALON_TROT,        { "Talon Trot",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.TalonTrot") } },
+    { ABILITY_11_TURBO_TALON,       { "Turbo Talon",    CVAR_RANDOMIZER_SETTING("Loadout.Abilities.TurboTalon") } },
+    { ABILITY_12_WONDERWING,        { "Wonderwing",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Wonderwing") } },
+};
+
+std::map<item_e, std::pair<const char*, const char*>> itemLoadoutMap = {
+    { ITEM_D_EGGS,          { "Blue Eggs",      CVAR_RANDOMIZER_SETTING("Loadout.Items.BlueEggs") } },
+    { ITEM_F_RED_FEATHER,   { "Red Feathers",   CVAR_RANDOMIZER_SETTING("Loadout.Items.RedFeathers") } },
+    { ITEM_10_GOLD_FEATHER, { "Gold Feathers",  CVAR_RANDOMIZER_SETTING("Loadout.Items.GoldFeathers") } },
+};
+    // clang-format on
 
 void Rando::Logic::InitializeSaveData(SaveData* saveData) {
     // RandoSaveCheck - Initialize
@@ -44,5 +82,19 @@ void Rando::Logic::GenerateSaveData(SaveData* saveData) {
         };
 
         saveData->shipSaveData.randoSaveData.randoSaveCheck[object.randoCheckId] = randoSaveCheck;
+    }
+}
+
+void Rando::Logic::GrantStartingLoadout() {
+    for (auto& [ability, abilityInfo] : abilityLoadoutMap) {
+        if (ability == ABILITY_A_HOLD_A_JUMP_HIGHER || CVarGetInteger(abilityInfo.second, 0)) {
+            ability_setLearned(ability, true);
+            ability_setHasUsed(ability);
+        }
+    }
+    for (auto& [item, itemInfo] : itemLoadoutMap) {
+        if (CVarGetInteger(itemInfo.second, 0)) {
+            item_setMaxCount(item);
+        }
     }
 }
