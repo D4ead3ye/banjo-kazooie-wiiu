6a610887176e7dc0e27b6bf23ec533a7d063baf9
Caladius <clatini88@gmail.com>
2026-06-04 08:45:41 -0400

    Add 1st Note Door ability to load out

--- src/port/Rando/Logic/GenerateSaveData.cpp
+++ src/port/Rando/Logic/GenerateSaveData.cpp
@@ -16,25 +16,26 @@ void fileProgressFlag_set(enum file_progress_e index, s32 set);
 
 // clang-format off
 std::map<ability_e, std::pair<const char*, const char*>> abilityLoadoutMap = {
-    { ABILITY_0_BARGE,              { "Beak Barge",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBarge") } },
-    { ABILITY_1_BEAK_BOMB,          { "Beak Bomb",      CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBomb") } },
-    { ABILITY_2_BEAK_BUSTER,        { "Beak Buster",    CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBuster") } },
-    { ABILITY_3_CAMERA_CONTROL,     { "Camera Control", CVAR_RANDOMIZER_SETTING("Loadout.Abilities.CameraControl") } },
-    { ABILITY_4_CLAW_SWIPE,         { "Claw Swipe",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.ClawSwipe") } },
-    { ABILITY_5_CLIMB,              { "Climb",          CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Climb") } },
-    { ABILITY_6_EGGS,               { "Eggs",           CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Eggs") } },
-    { ABILITY_7_FEATHERY_FLAP,      { "Feathery Flap",  CVAR_RANDOMIZER_SETTING("Loadout.Abilities.FeatheryFlap") } },
-    { ABILITY_8_FLAP_FLIP,          { "Flap Flip",      CVAR_RANDOMIZER_SETTING("Loadout.Abilities.FlapFlip") } },
-    { ABILITY_9_FLIGHT,             { "Flight",         CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Flight") } },
-    { ABILITY_A_HOLD_A_JUMP_HIGHER, { "Jump Higher",    CVAR_RANDOMIZER_SETTING("Loadout.Abilities.JumpHigher") } },
-    { ABILITY_B_RATATAT_RAP,        { "Rat-a-tat Rap",  CVAR_RANDOMIZER_SETTING("Loadout.Abilities.RatatatRap") } },
-    { ABILITY_C_ROLL,               { "Roll",           CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Roll") } },
-    { ABILITY_D_SHOCK_JUMP,         { "Shock Jump",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.ShockJump") } },
-    { ABILITY_E_WADING_BOOTS,       { "Wading Boots",   CVAR_RANDOMIZER_SETTING("Loadout.Abilities.WadingBoots") } },
-    { ABILITY_F_DIVE,               { "Dive",           CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Dive") } },
-    { ABILITY_10_TALON_TROT,        { "Talon Trot",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.TalonTrot") } },
-    { ABILITY_11_TURBO_TALON,       { "Turbo Talon",    CVAR_RANDOMIZER_SETTING("Loadout.Abilities.TurboTalon") } },
-    { ABILITY_12_WONDERWING,        { "Wonderwing",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Wonderwing") } },
+    { ABILITY_0_BARGE,              { "Beak Barge",         CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBarge") } },
+    { ABILITY_1_BEAK_BOMB,          { "Beak Bomb",          CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBomb") } },
+    { ABILITY_2_BEAK_BUSTER,        { "Beak Buster",        CVAR_RANDOMIZER_SETTING("Loadout.Abilities.BeakBuster") } },
+    { ABILITY_3_CAMERA_CONTROL,     { "Camera Control",     CVAR_RANDOMIZER_SETTING("Loadout.Abilities.CameraControl") } },
+    { ABILITY_4_CLAW_SWIPE,         { "Claw Swipe",         CVAR_RANDOMIZER_SETTING("Loadout.Abilities.ClawSwipe") } },
+    { ABILITY_5_CLIMB,              { "Climb",              CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Climb") } },
+    { ABILITY_6_EGGS,               { "Eggs",               CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Eggs") } },
+    { ABILITY_7_FEATHERY_FLAP,      { "Feathery Flap",      CVAR_RANDOMIZER_SETTING("Loadout.Abilities.FeatheryFlap") } },
+    { ABILITY_8_FLAP_FLIP,          { "Flap Flip",          CVAR_RANDOMIZER_SETTING("Loadout.Abilities.FlapFlip") } },
+    { ABILITY_9_FLIGHT,             { "Flight",             CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Flight") } },
+    { ABILITY_A_HOLD_A_JUMP_HIGHER, { "Jump Higher",        CVAR_RANDOMIZER_SETTING("Loadout.Abilities.JumpHigher") } },
+    { ABILITY_B_RATATAT_RAP,        { "Rat-a-tat Rap",      CVAR_RANDOMIZER_SETTING("Loadout.Abilities.RatatatRap") } },
+    { ABILITY_C_ROLL,               { "Roll",               CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Roll") } },
+    { ABILITY_D_SHOCK_JUMP,         { "Shock Jump",         CVAR_RANDOMIZER_SETTING("Loadout.Abilities.ShockJump") } },
+    { ABILITY_E_WADING_BOOTS,       { "Wading Boots",       CVAR_RANDOMIZER_SETTING("Loadout.Abilities.WadingBoots") } },
+    { ABILITY_F_DIVE,               { "Dive",               CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Dive") } },
+    { ABILITY_10_TALON_TROT,        { "Talon Trot",         CVAR_RANDOMIZER_SETTING("Loadout.Abilities.TalonTrot") } },
+    { ABILITY_11_TURBO_TALON,       { "Turbo Talon",        CVAR_RANDOMIZER_SETTING("Loadout.Abilities.TurboTalon") } },
+    { ABILITY_12_WONDERWING,        { "Wonderwing",         CVAR_RANDOMIZER_SETTING("Loadout.Abilities.Wonderwing") } },
+    { ABILITY_13_1ST_NOTEDOOR,      { "First Note Door",    CVAR_RANDOMIZER_SETTING("Loadout.Abilities.NoteDoor") } },
 };
 
 std::map<item_e, std::pair<const char*, const char*>> itemLoadoutMap = {
@@ -144,7 +145,7 @@ void Rando::Logic::GenerateSaveData(SaveData* saveData) {
 
 void Rando::Logic::GrantStartingLoadout() {
     for (auto& [ability, abilityInfo] : abilityLoadoutMap) {
-        if (ability == ABILITY_A_HOLD_A_JUMP_HIGHER || CVarGetInteger(abilityInfo.second, 0)) {
+        if (ability == ABILITY_A_HOLD_A_JUMP_HIGHER || ability == ABILITY_13_1ST_NOTEDOOR || CVarGetInteger(abilityInfo.second, 0)) {
             ability_setLearned(ability, true);
             ability_setHasUsed(ability);
         }
