44fb15ea38a6fcf1026e1c3b35f7e75c5cb2b93a
Caladius <clatini88@gmail.com>
2026-06-14 22:00:49 -0400

    Fixes

--- src/port/enhancements/events/hooks/list/GameEvent.h
+++ src/port/enhancements/events/hooks/list/GameEvent.h
@@ -2,44 +2,17 @@
 
 #include <libultraship/bridge/eventsbridge.h>
 
-// clang-format off
-DEFINE_EVENT(OnGameSave, 
-	void* saveData;
-)
-
-DEFINE_EVENT(OnGameLoad,
-	int32_t fileNum;
-)
-
-DEFINE_EVENT(OnSaveFileLoad,
-	int32_t fileNum;
-	void* saveBuffer;
-	int32_t result;
-)
-
-DEFINE_EVENT(OnWarpDispatch,
-	int32_t warpId;
-	int32_t warpDest;
-)
-
-DEFINE_EVENT(OnSaveFileSave, void* saveBuffer; int32_t fileNum; int32_t * result;)
-
-DEFINE_EVENT(MapTransitionEnd, GameMap map; s32 exitID;);
-
 DEFINE_EVENT(OnActorDestroy, Actor* actor;);
-
+DEFINE_EVENT(OnGameSave, int32_t fileNum;)
+DEFINE_EVENT(OnGameLoad, int32_t fileNum;)
 DEFINE_EVENT(OnPropInit, Prop* propPtr;);
+DEFINE_EVENT(OnSaveFileLoad, int32_t fileNum; void* saveBuffer; int32_t result;)
+DEFINE_EVENT(OnSaveFileSave, void* saveBuffer; int32_t fileNum; int32_t* result;)
+// Identifies which warp_* dispatcher is firing OnWarpResolveDest. Keep values
+// stable so listener case statements keep matching across refactors.
+typedef enum WarpId {
+    WARP_ID_SM_EXIT_BANJOS_HOUSE = 1,
+    WARP_ID_LAIR_ENTER_MM_LOBBY_FROM_SM_LEVEL = 2,
+} WarpId;
 
-DEFINE_EVENT(OnSetJiggyList,
-	int32_t levelId;
-)
-
-DEFINE_EVENT(OnSaveClear,
-	void* result;
-)
-
-DEFINE_EVENT(OnGetLevelSpecificFlag,
-	int32_t flagId;
-	int32_t result;
-)
-// clang-format on
+DEFINE_EVENT(OnWarpResolveDest, int32_t warpId; int32_t defaultDest; int32_t bkcfOverride; int32_t* dest;);
