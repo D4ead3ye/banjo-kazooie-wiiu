f9210b37af5ab5e8257c273d6c95c278506cbf6c
Fredomato <86324131+Fredomato@users.noreply.github.com>
2026-06-22 01:58:29 +0100

    misc logic and tumblar change

--- src/port/Rando/MiscBehavior/WorldState.cpp
+++ src/port/Rando/MiscBehavior/WorldState.cpp
@@ -151,15 +151,14 @@ void Rando::MiscBehavior::InitWorldStateBehavior() {
 
         map_e currentMap = gsworld_getMap();
 
-        for (auto& saveCheck : RANDO_SAVE_CHECKS) {
-            if (Rando::StaticData::Checks[saveCheck.shuffledCheckId].randoCheckType != RCTYPE_JIGGY) {
+        for (auto& [id, data] : Rando::StaticData::Checks) {
+            if (data.randoCheckType != RCTYPE_JIGGY) {
                 continue;
             }
 
-            if (saveCheck.randoCollectionId == ev->jiggyId) {
+            if (data.collectionId == ev->jiggyId) {
                 if (ev->jiggyId == JIGGY_5D_MMM_NAPPER) {
-                    if (currentMap == MAP_26_MMM_NAPPERS_ROOM &&
-                        saveCheck.randoCheckId != RC_MMM_JIGGY_MANSION_TABLE) {
+                    if (currentMap == MAP_26_MMM_NAPPERS_ROOM && id != RC_MMM_JIGGY_MANSION_TABLE) {
                         event->Cancelled = true;
                         ev->result = RANDO_SAVE_CHECKS[RC_MMM_JIGGY_MANSION_TABLE].obtained;
                         break;
@@ -172,44 +171,12 @@ void Rando::MiscBehavior::InitWorldStateBehavior() {
                         break;
                     }
                 }
-
-                // TODO: Delete before converge
-                // switch (ev->jiggyId) {
-                //     case JIGGY_5_MM_HUTS:
-                //     case JIGGY_06_MM_RUINS:
-                //     case JIGGY_07_MM_HILL:
-                //     case JIGGY_8_MM_ORANGE_PADS:
-                //     case JIGGY_0C_TTC_LIGHTHOUSE:
-                //     case JIGGY_0E_TTC_ALCOVE_2:
-                //     case JIGGY_0F_TTC_POOL:
-                //     case JIGGY_10_TTC_SANDCASTLE:
-                //     case JIGGY_11_TTC_RED_X:
-                //     case JIGGY_12_TTC_NIPPER:
-                //     case JIGGY_1B_CC_TOOTH:
-                //     case JIGGY_1C_CC_RINGS:
-                //     case JIGGY_20_BGS_ELEVATED_WALKWAY:
-                //     case JIGGY_21_BGS_PINKEGG:
-                //     case JIGGY_23_BGS_HUTS:
-                //     case JIGGY_26_BGS_TANKTUP:
-                //     case JIGGY_27_BGS_TIPTUP:
-                //     case JIGGY_2B_FP_PIPE:
-                //     case JIGGY_2F_FP_XMAS_TREE:
-                //     case JIGGY_31_FP_SIR_SLUSH:
-                //     case JIGGY_33_LAIR_1ST_JIGGY:
-                //     case JIGGY_37_LAIR_BGS_WITCH_SWITCH:
-                //     event->Cancelled = true;
-                //     ev->result = saveCheck.obtained;
-                //     return;
-                //     default:
-                //         break;
-                // }              
-
-                event->Cancelled = true;
-                //ev->result = RANDO_SAVE_CHECKS[saveCheck.shuffledCheckId].obtained;
-                ev->result = saveCheck.obtained;
-                //SPDLOG_INFO("Jiggy {} result is {}", std::to_string(ev->jiggyId), std::to_string(ev->result));
-                break;
             }
+
+            event->Cancelled = true;
+            ev->result = RANDO_SAVE_CHECKS[id].obtained;
+            SPDLOG_INFO("Jiggy {} result is {}", std::to_string(ev->jiggyId), std::to_string(ev->result));
+            break;
         }
     })
 
